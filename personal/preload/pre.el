547a4081ca4d8dba1cd55835bf60ec3b6a55aee0
Manikandan Sundararajan <mani@hey.com>
2021-02-03 17:16:32 -0500

    Set doom-horizon to be the theme

--- personal/preload/pre.el
+++ personal/preload/pre.el
@@ -1,1 +1,8 @@
-(setq prelude-theme 'doom-laserwave)
+;; (setq prelude-theme 'doom-horizon-theme)
+(setq prelude-theme 'doom-horizon)
+(setq prelude-minimalistic-ui t)
+
+;; This is equivalent to
+;; (global-nlinum-mode -1)
+;; (menu-bar-mode -1)
+;; but is better since those elements don't appear temporarily
